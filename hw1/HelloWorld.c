859f40dc0775afa7c612c715ee27858a7d9c766b
max-hug <maxhug@u.northwestern.edu>
2025-01-08 14:56:44 -0600

    hw1

---
+++ hw1/HelloWorld.c
@@ -0,0 +1,13 @@
+/* 
+HelloWorld.c
+Max Hughes
+ME 333 HW 1
+*/
+
+#include <stdio.h>
+
+int main(void){
+    printf("Hello World!\n");
+    return 0;
+}
+
---
+++ hw1/build/CMakeFiles/my_program.dir/HelloWorld.c.obj.d
@@ -0,0 +1,12 @@
+CMakeFiles/my_program.dir/HelloWorld.c.obj: \
+ C:\Users\maxhu\Documents\VS_Code\me_333\hw1\HelloWorld.c \
+ C:/msys64/ucrt64/include/stdio.h \
+ C:/msys64/ucrt64/include/corecrt_stdio_config.h \
+ C:/msys64/ucrt64/include/corecrt.h C:/msys64/ucrt64/include/_mingw.h \
+ C:/msys64/ucrt64/include/_mingw_mac.h \
+ C:/msys64/ucrt64/include/_mingw_secapi.h \
+ C:/msys64/ucrt64/include/vadefs.h \
+ C:/msys64/ucrt64/include/sdks/_mingw_ddk.h \
+ C:/msys64/ucrt64/include/_mingw_off_t.h \
+ C:/msys64/ucrt64/include/swprintf.inl \
+ C:/msys64/ucrt64/include/sec_api/stdio_s.h
